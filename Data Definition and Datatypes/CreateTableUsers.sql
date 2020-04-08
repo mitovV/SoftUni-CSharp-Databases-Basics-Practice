2a189d047dadce8a0c4ff34898ec6ea364610098
CodingMania <vasilmitovv@gmail.com>
2020-04-08 15:08:48 +0300

    Update

--- Data Definition and Datatypes/CreateTableUsers.sql
+++ Data Definition and Datatypes/CreateTableUsers.sql
@@ -2,23 +2,34 @@ CREATE TABLE Users(
 	Id BIGINT PRIMARY KEY IDENTITY,
 	Username  VARCHAR(30) UNIQUE NOT NULL, 
 	[Password] VARCHAR(26) NOT NULL,
+	CHECK(LEN([Password]) >= 5),
 	ProfilePicture VARBINARY(MAX),
 	CHECK(DATALENGTH(ProfilePicture) <= 921600),
-	LastLoginTime DATETIME,
+	LastLoginTime DATETIME2,
 	IsDeleted BIT NOT NULL
 )
 
 INSERT INTO Users(Username, [Password], ProfilePicture,LastLoginTime, IsDeleted)
 VALUES
-				 ('Pesho', '123', NULL, NULL, 0),
-				 ('Gosho', '123', NULL, NULL,1),
-				 ('Ivan', '123', NULL, NULL, 0),
-				 ('Gergana', '123', NULL, NULL, 1),
-				 ('Kiril', '123', NULL, NULL, 0)
+				 ('Pesho', '12345', NULL, NULL, 0),
+				 ('Gosho', '123456', NULL, NULL,1),
+				 ('Ivan', '1234567', NULL, NULL, 0),
+				 ('Gergana', '123456', NULL, NULL, 1),
+				 ('Kiril', '123456', NULL, NULL, 0)
 
 ALTER TABLE Users
-DROP CONSTRAINT PK__Users__3214EC07F96D8B2B
+DROP CONSTRAINT PK__Users__3214EC079142EC28
 
 ALTER TABLE Users
 ADD CONSTRAINT PK_CompositeIdUsername
 PRIMARY KEY (Id, Username) 
+
+ALTER TABLE Users
+ADD CONSTRAINT DF_LoginTime
+DEFAULT GETDATE() FOR LastLoginTime
+
+INSERT INTO Users (Username, [Password], ProfilePicture, IsDeleted)
+VALUES 
+				  ('KiriLl', '123456', NULL, 0)
+
+SELECT * FROM Users
