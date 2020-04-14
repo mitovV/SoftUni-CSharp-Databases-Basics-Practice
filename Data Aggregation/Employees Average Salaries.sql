fdc4b46d38ec30cc918a1dd21648f90afd0fbf33
CodingMania <vasilmitovv@gmail.com>
2020-04-14 17:50:56 +0300

    Initial commit

---
+++ Data Aggregation/Employees Average Salaries.sql
@@ -0,0 +1,14 @@
+SELECT * INTO NewEmployeesTable 
+  FROM Employees
+ WHERE Salary > 30000
+
+ DELETE FROM NewEmployeesTable 
+       WHERE ManagerID = 42
+
+UPDATE NewEmployeesTable
+   SET Salary += 5000
+ WHERE DepartmentID = 1
+
+  SELECT DepartmentID, AVG(Salary) AS AverageSalary
+    FROM NewEmployeesTable
+GROUP BY DepartmentID
