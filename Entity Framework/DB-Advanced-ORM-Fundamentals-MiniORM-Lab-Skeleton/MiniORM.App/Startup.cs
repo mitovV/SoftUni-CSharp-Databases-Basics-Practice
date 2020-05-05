aeb49ff6101758eb19af86a5e38d2007d051a88e
CodingMania <vasilmitovv@gmail.com>
2020-05-05 15:30:48 +0300

    Initial commit

---
+++ Entity Framework/DB-Advanced-ORM-Fundamentals-MiniORM-Lab-Skeleton/MiniORM.App/Startup.cs
@@ -0,0 +1,29 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var connectionString = @"Server=CODINGMANIA\SQLEXPRESS;Database=MiniORM;Integrated Security=true;";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirsName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirsName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
