da2a0a747a0105e702bc23cbe8089a1c08ffd349
CodingMania <vasilmitovv@gmail.com>
2020-05-23 11:21:21 +0300

    Initial commit

---
+++ Entity Framework/Extensible Markup Language - XML/ProductShop - Skeleton/ProductShop/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace ProductShop
+{
+    using System;
+    using System.IO;
+    using System.Xml.Serialization;
+
+    using Data;
+    using Dtos.Import;
+
+    using AutoMapper;
+    using ProductShop.Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile<ProductShopProfile>());
+
+            using (var db = new ProductShopContext())
+            {
+                var data = File.ReadAllText("../../../Datasets/users.xml");
+
+                var result = ImportUsers(db, data);
+
+                Console.WriteLine(result);
+            }
+
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(ImportUserDto[]),new XmlRootAttribute("Users"));
+
+            ImportUserDto[] userDtos;
+
+            using (var reader = new StringReader(inputXml))
+            {
+              userDtos = (ImportUserDto[])serializer.Deserialize(reader);
+            }
+
+            var users = Mapper.Map<User[]>(userDtos);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+    }
+}
