99d231685f18010a3c937b458bfb0c2473e0a825
CodingMania <vasilmitovv@gmail.com>
2020-05-23 19:48:16 +0300

    Solved task 9

---
+++ Entity Framework/Extensible Markup Language - XML/CarDealer - Skeleton/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace CarDealer
+{
+    using Dtos.Import;
+    using Models;
+
+    using AutoMapper;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            this.CreateMap<ImportSupplierDto, Supplier>();
+        }
+    }
+}
---
+++ Entity Framework/Extensible Markup Language - XML/CarDealer - Skeleton/CarDealer/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace CarDealer
+{
+    using System;
+    using System.IO;
+    using System.Xml.Serialization;
+
+    using Data;
+    using Dtos.Import;
+    using Models;
+
+    using AutoMapper;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            using (var db = new CarDealerContext())
+            {
+                var data = File.ReadAllText("../../../Datasets/suppliers.xml");
+
+                var result = ImportSuppliers(db, data);
+
+                Console.WriteLine(result);
+
+            }
+
+        }
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(ImportSupplierDto[]), new XmlRootAttribute("Suppliers"));
+
+            ImportSupplierDto[] supplierDtos;
+
+            using (var reader = new StringReader(inputXml))
+            {
+                supplierDtos = (ImportSupplierDto[])serializer.Deserialize(reader); 
+            }
+
+            var suppliers = Mapper.Map<Supplier[]>(supplierDtos);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
+        }
+    }
+}
