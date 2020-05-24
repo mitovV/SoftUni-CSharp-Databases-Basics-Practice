61f568d89f2ad3631442e61bae1dad89bfb7c8e8
CodingMania <vasilmitovv@gmail.com>
2020-05-24 18:54:12 +0300

    Solved task 17

--- Entity Framework/Extensible Markup Language - XML/CarDealer - Skeleton/CarDealer/StartUp.cs
+++ Entity Framework/Extensible Markup Language - XML/CarDealer - Skeleton/CarDealer/StartUp.cs
@@ -24,7 +24,7 @@
             {
                 var data = File.ReadAllText("../../../Datasets/sales.xml");
 
-                var result = GetLocalSuppliers(db);
+                var result = GetCarsWithTheirListOfParts(db);
 
                 Console.WriteLine(result);
 
@@ -246,6 +246,43 @@
                 namespaces.Add("", "");
 
                 serializer.Serialize(writer, suppliers, namespaces);
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var cars = context
+                .Cars
+                .Select(c => new ExportCarWithPartDto()
+                {
+                    Make = c.Make,
+                    Model = c.Model,
+                    TravelledDistance = c.TravelledDistance,
+                    Parts = c.PartCars.Select(pc => new ExportPartDto()
+                    {
+                        Name = pc.Part.Name,
+                        Price = pc.Part.Price
+                    })
+                    .OrderByDescending(p => p.Price)
+                    .ToArray()
+                })
+                .OrderByDescending(c => c.TravelledDistance)
+                .ThenBy(c => c.Model)
+                .Take(5)
+                .ToArray();
+
+            var sb = new StringBuilder();
+
+            using (var writer = new StringWriter(sb))
+            {
+                var serializer = new XmlSerializer(typeof(ExportCarWithPartDto[]), new XmlRootAttribute("cars"));
+
+                var namespaces = new XmlSerializerNamespaces();
+                namespaces.Add("", "");
+
+                serializer.Serialize(writer, cars, namespaces);
             }
 
             return sb.ToString().Trim();
