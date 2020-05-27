749cbf9be7535ecfa13736bc8d2cc5cae5e301e0
CodingMania <vasilmitovv@gmail.com>
2020-05-27 16:48:08 +0300

    Solved task 1

---
+++ Entity Framework/Exams practice/C# DB Advanced Exam - 13 Dec 2019/BookShop/Data/Models/Author.cs
@@ -0,0 +1,28 @@
+﻿namespace BookShop.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength =3)]
+        public  string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        public ICollection<AuthorBook> AuthorsBooks { get; set; }
+    }
+}
---
+++ Entity Framework/Exams practice/C# DB Advanced Exam - 13 Dec 2019/BookShop/Data/Models/AuthorBook.cs
@@ -0,0 +1,20 @@
+﻿namespace BookShop.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class AuthorBook
+    {
+        [Required]
+        [Key, Column(Order = 0)]
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int BookId { get; set; }
+
+        public Book Book { get; set; }
+
+    }
+}
