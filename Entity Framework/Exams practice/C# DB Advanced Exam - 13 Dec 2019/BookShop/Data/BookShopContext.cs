5accd4efa2837d34ffbae836b8a2b67a31651884
CodingMania <vasilmitovv@gmail.com>
2020-05-27 18:32:27 +0300

    Added composite key

--- Entity Framework/Exams practice/C# DB Advanced Exam - 13 Dec 2019/BookShop/Data/BookShopContext.cs
+++ Entity Framework/Exams practice/C# DB Advanced Exam - 13 Dec 2019/BookShop/Data/BookShopContext.cs
@@ -2,12 +2,20 @@
 {
     using Microsoft.EntityFrameworkCore;
 
+    using Data.Models;
+
     public class BookShopContext : DbContext
     {
         public BookShopContext() { }
 
         public BookShopContext(DbContextOptions options)
             : base(options) { }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<AuthorBook> AuthorBooks { get; set; }
+
+        public DbSet<Book> Books { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -20,6 +28,10 @@
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<AuthorBook>(e =>
+            {
+                e.HasKey(k => new { k.AuthorId, k.BookId });
+            });
         }
     }
 }
--- Entity Framework/Exams practice/C# DB Advanced Exam - 13 Dec 2019/BookShop/Data/Models/AuthorBook.cs
+++ Entity Framework/Exams practice/C# DB Advanced Exam - 13 Dec 2019/BookShop/Data/Models/AuthorBook.cs
@@ -1,20 +1,13 @@
 ﻿namespace BookShop.Data.Models
 {
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-
     public class AuthorBook
     {
-        [Required]
-        [Key, Column(Order = 0)]
         public int AuthorId { get; set; }
 
         public Author Author { get; set; }
 
-        [Key, Column(Order = 1)]
         public int BookId { get; set; }
 
         public Book Book { get; set; }
-
     }
 }
