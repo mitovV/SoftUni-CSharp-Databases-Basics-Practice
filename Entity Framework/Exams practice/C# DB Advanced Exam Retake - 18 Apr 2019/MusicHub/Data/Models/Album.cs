e9b54d238d3f36ed095176d87dda95908ddbf25f
CodingMania <vasilmitovv@gmail.com>
2020-06-01 14:16:20 +0300

    Two test fails on first task

---
+++ Entity Framework/Exams practice/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,29 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+
+    public class Album
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price => this.Songs.Sum(s => s.Price);
+
+        public int ProducerId { get; set; }
+
+        [Required]
+        public Producer Producer { get; set; }
+
+        public ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ Entity Framework/Exams practice/C# DB Advanced Exam Retake - 18 Apr 2019/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,42 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using Enums;
+
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public DateTime CreatedOn  { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public Album Album { get; set; }
+
+        [Required]
+        public int WriterId { get; set; }
+
+        public Writer Writer { get; set; }
+
+        [Required]
+        [Range(typeof(string), "0", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+
+        public ICollection<SongPerformer> SongPerformers { get; set; }
+    }
+}
